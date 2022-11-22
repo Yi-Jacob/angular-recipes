4f409d1e3a967a9d1d8712163f0c0cceba4b260c
Yi-Jacob <jakeyi00@gmail.com>
2022-11-21 17:08:53 -0800

    updated sign in api link

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -49,7 +49,7 @@ export class AuthService {
   login(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBOioBm8nHxoaOGcAmrlnZYv3ELqrGAYGA',
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBOioBm8nHxoaOGcAmrlnZYv3ELqrGAYGA',
         {
           email: email,
           password: password,
